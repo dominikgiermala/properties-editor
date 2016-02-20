1b140b13ce7dedd1e26016ae502cdbf92b8a58f7
Dominik <giermalad@gmail.com>
2016-02-20 23:29:35 +0000

    dehardcoded settings, extracted to user's settings files

--- properties_editor.py
+++ properties_editor.py
@@ -6,7 +6,8 @@ from .lib.pyjavaproperties import Properties
 
 class PropertiesEditorCommand(sublime_plugin.WindowCommand):
   def run(self):
-    self.window.show_input_panel("Property key:", '', self.on_key_put, None, None)
+    if self.load_and_validate_settings():
+      self.window.show_input_panel("Property key:", '', self.on_key_put, None, None)
 
   def on_key_put(self, key):
     if key and key.strip():
@@ -18,12 +19,24 @@ class PropertiesEditorCommand(sublime_plugin.WindowCommand):
       self.edit_properties(self.key, value)
 
   def edit_properties(self, key, value):
-    os.chdir("/home/dominik/git/al/ei-i18n-messages/src/main/resources")
+    os.chdir(self.properties_dir)
     confirmation_message = "Property " + self.key + " set to " + value + " In files:\n"
-    for file in glob.glob("messages-locale_*.properties"):
+    for file in glob.glob(self.files_regex):
       p = Properties()
       p.load(open(file))
       p[key] = value
       p.store(open(file, 'w'))
       confirmation_message += file + "\n"
     sublime.message_dialog(confirmation_message)
+
+  def load_and_validate_settings(self):
+    config_file_name = "PropertiesEditor.sublime-settings"
+    settings = sublime.load_settings(config_file_name)
+    self.properties_dir = settings.get('properties_dir')
+    self.files_regex = settings.get('files_regex')
+    is_valid = self.properties_dir and self.properties_dir.strip() and self.files_regex and self.files_regex.strip()
+    if not is_valid:
+      sublime.message_dialog("Properties Editor needs congiuration to work correctly.\n" +
+        "Please set path to properties files regex in file " + config_file_name)
+    return is_valid
+    
