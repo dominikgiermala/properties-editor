4fa2fee9b990c1b8d8d677750106533023eb8f68
Dominik <giermalad@gmail.com>
2016-03-05 21:23:14 +0000

    adding property in one step - key and value in 1 input, improved confirmation messages

--- src/properties_editor.py
+++ src/properties_editor.py
@@ -1,3 +1,4 @@
+import os
 import sublime
 import sublime_plugin
 from .lib.pyjavaproperties import Properties
@@ -6,16 +7,15 @@ class AddEditPropertyCommand(sublime_plugin.WindowCommand):
   def run(self, paths = []):
     # TODO: validate if *.properties file
     self.paths = paths
-    self.window.show_input_panel("Property key:", '', self.on_key_put, None, None)
+    self.window.show_input_panel("Property to add/edit:", '', self.on_property_put, None, None)
 
-  def on_key_put(self, key):
-    if key and key.strip():
-      self.key = key
-      self.window.show_input_panel("Property value:", '', self.on_value_put, None, None)
-
-  def on_value_put(self, value):
-    if value and value.strip():
-      self.edit_properties(self.key, value)
+  def on_property_put(self, property):
+    if property and property.strip() and '=' in property:
+      key_value = property.split('=', 1)
+      self.key = key_value[0]
+      self.value = key_value[1]
+      if self.key and self.value:
+        self.edit_properties(self.key, self.value)
 
   def edit_properties(self, key, value):
     files_without_key = []
@@ -24,14 +24,19 @@ class AddEditPropertyCommand(sublime_plugin.WindowCommand):
       p = Properties()
       p.load(open(file))
       if p.getProperty(key):
-        files_with_key.append(file)
+        files_with_key.append(os.path.basename(file))
       else:
-        files_without_key.append(file)
+        files_without_key.append(os.path.basename(file))
       p[key] = value
       p.store(open(file, 'w'))
-    confirmation_message = "Property " + key + "=" + value + " was: " 
-    confirmation_message += "\nAdded in files:\n" + "\n".join(files_without_key)
-    confirmation_message += "\n\nEdited in files:\n" + "\n".join(files_with_key)
+    self.display_confirmation_message(files_without_key, files_with_key)
+
+  def display_confirmation_message(self, files_without_key, files_with_key):
+    confirmation_message = "Property " + self.key + "=" + self.value + " was: " 
+    if files_without_key:
+      confirmation_message += "\nAdded in files:\n" + "\n".join(files_without_key)
+    if files_with_key:
+      confirmation_message += "\n\nEdited in files:\n" + "\n".join(files_with_key)
     sublime.message_dialog(confirmation_message)
 
 
@@ -39,10 +44,11 @@ class RemovePropertyCommand(sublime_plugin.WindowCommand):
   def run(self, paths = []):
     # TODO: validate if *.properties file
     self.paths = paths
-    self.window.show_input_panel("Property key:", '', self.on_key_put, None, None)
+    self.window.show_input_panel("Property key to remove:", '', self.on_key_put, None, None)
 
   def on_key_put(self, key):
     if key and key.strip():
+      self.key = key
       self.remove_property(key, self.paths)
 
   def remove_property(self, key, paths):
@@ -53,14 +59,22 @@ class RemovePropertyCommand(sublime_plugin.WindowCommand):
       p.load(open(file))
       if p.getProperty(key):
         p.removeProperty(key)
-        files_with_key.append(file)
+        files_with_key.append(os.path.basename(file))
         p.store(open(file, 'w'))
       else:
-        files_without_key.append(file)
-    confirmation_message = "Property " + key + " was: "
-    confirmation_message += "\nRemoved in files:\n" + "\n".join(files_with_key)
-    confirmation_message += "\n\nNot found in files:\n" + "\n".join(files_without_key)
-    sublime.message_dialog(confirmation_message)
+        files_without_key.append(os.path.basename(file))
+    self.display_confirmation_message(files_without_key, files_with_key)
+
+  def display_confirmation_message(self, files_without_key, files_with_key):
+    confirmation_message = "Property with key " + self.key + " was: "
+    if files_with_key:
+      confirmation_message += "\nRemoved in files:\n" + "\n".join(files_with_key)
+    if files_without_key:
+      confirmation_message += "\n\nNot found in files:\n" + "\n".join(files_without_key)  
+    if files_without_key:
+      sublime.error_message(confirmation_message)
+    else:
+      sublime.message_dialog(confirmation_message)
 
 
 class RenameKeyCommand(sublime_plugin.WindowCommand):
@@ -90,11 +104,11 @@ class RenameKeyCommand(sublime_plugin.WindowCommand):
         if not p.getProperty(new_key):
           p[new_key] = p[old_key]
           p.removeProperty(old_key)
-          files_with_renamed_key.append(file)
+          files_with_renamed_key.append(os.path.basename(file))
         else:
-          files_with_new_key.append(file)
+          files_with_new_key.append(os.path.basename(file))
       else:
-        files_without_old_key.append(file)
+        files_without_old_key.append(os.path.basename(file))
       p.store(open(file, 'w'))
     self.display_confirmation_message(files_without_old_key, files_with_new_key, files_with_renamed_key)
 
