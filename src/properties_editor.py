7095cbecb3d329ec88ebe6b0dac724f3516550db
Dominik <giermalad@gmail.com>
2016-03-05 21:18:40 +0000

    possibility to rename property key

--- src/properties_editor.py
+++ src/properties_editor.py
@@ -2,7 +2,7 @@ import sublime
 import sublime_plugin
 from .lib.pyjavaproperties import Properties
 
-class UpdatePropertyCommand(sublime_plugin.WindowCommand):
+class AddEditPropertyCommand(sublime_plugin.WindowCommand):
   def run(self, paths = []):
     # TODO: validate if *.properties file
     self.paths = paths
@@ -30,8 +30,8 @@ class UpdatePropertyCommand(sublime_plugin.WindowCommand):
       p[key] = value
       p.store(open(file, 'w'))
     confirmation_message = "Property " + key + "=" + value + " was: " 
-    confirmation_message += "\nAdded in files:\n" + "".join(files_without_key)
-    confirmation_message += "\nEdited in files:\n" + "".join(files_with_key)
+    confirmation_message += "\nAdded in files:\n" + "\n".join(files_without_key)
+    confirmation_message += "\n\nEdited in files:\n" + "\n".join(files_with_key)
     sublime.message_dialog(confirmation_message)
 
 
@@ -58,7 +58,55 @@ class RemovePropertyCommand(sublime_plugin.WindowCommand):
       else:
         files_without_key.append(file)
     confirmation_message = "Property " + key + " was: "
-    confirmation_message += "\nRemoved in files:\n" + "".join(files_with_key)
-    confirmation_message += "\nNot found in files:\n" + "".join(files_without_key)
+    confirmation_message += "\nRemoved in files:\n" + "\n".join(files_with_key)
+    confirmation_message += "\n\nNot found in files:\n" + "\n".join(files_without_key)
     sublime.message_dialog(confirmation_message)
-    
+
+
+class RenameKeyCommand(sublime_plugin.WindowCommand):
+  def run(self, paths = []):
+    # TODO: validate if *.properties file
+    self.paths = paths
+    self.window.show_input_panel("Key to rename:", '', self.on_old_key_put, None, None)
+
+  def on_old_key_put(self, old_key):
+    if old_key and old_key.strip():
+      self.old_key = old_key
+      self.window.show_input_panel("New key:", '', self.on_new_key_put, None, None)
+
+  def on_new_key_put(self, new_key):
+    if new_key and new_key.strip():
+      self.new_key = new_key
+      self.rename_key(self.old_key, self.new_key)
+
+  def rename_key(self, old_key, new_key):
+    files_without_old_key = []
+    files_with_new_key = []
+    files_with_renamed_key = []
+    for file in self.paths:
+      p = Properties()
+      p.load(open(file))
+      if p.getProperty(old_key):
+        if not p.getProperty(new_key):
+          p[new_key] = p[old_key]
+          p.removeProperty(old_key)
+          files_with_renamed_key.append(file)
+        else:
+          files_with_new_key.append(file)
+      else:
+        files_without_old_key.append(file)
+      p.store(open(file, 'w'))
+    self.display_confirmation_message(files_without_old_key, files_with_new_key, files_with_renamed_key)
+
+  def display_confirmation_message(self, files_without_old_key, files_with_new_key, files_with_renamed_key):
+    confirmation_message = "Key " + self.old_key + " was: " 
+    if files_with_renamed_key:
+      confirmation_message += "\nRenamed in files:\n" + "\n".join(files_with_renamed_key)
+    if files_without_old_key:
+      confirmation_message += "\n\nNot found in files:\n" + "\n".join(files_without_old_key)
+    if files_with_new_key:
+      confirmation_message += "\n\nKey " + self.new_key + " already exists in files:\n" + "\n".join(files_with_new_key)
+    if files_without_old_key or files_with_new_key:
+      sublime.error_message(confirmation_message)
+    else:
+      sublime.message_dialog(confirmation_message)
